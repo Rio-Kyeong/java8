b4d65e8f312d8eacd8e4c22b433301d6e0b08f9d
Leo <drd9811@naver.com>
2022-11-24 23:45:43 +0900

    Use Stream

--- src/main/java/me/leo/java8/UseStream.java
+++ src/main/java/me/leo/java8/UseStream.java
@@ -1,7 +1,14 @@
 package me.leo.java8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UseStream {
     public static void main(String[] args) {
-
+        List<String> names = new ArrayList<>();
+        names.add("Leo");
+        names.add("whiteship");
+        names.add("toby");
+        names.add("foo");
     }
 }
